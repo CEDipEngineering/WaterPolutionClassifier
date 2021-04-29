7f5f77927f79e7d4e7dce60f14a4b116507ffd24
Carlos Dip <cadudip@hotmail.com>
2021-04-29 11:43:20 -0300

    Finished implementing contour

--- iqa_interpolator.py
+++ iqa_interpolator.py
@@ -24,17 +24,31 @@ class IQA_Interpolator():
         self.indicators['Oxigênio Dissolvido'] = pd.read_csv('csv_iqa/o2dissolvido.csv', delimiter=',', decimal='.', header=None)
         self.indicatorFunctions = {}
         self.indicatorFunctions['Coliformes Termotolerantes'] = lambda x: np.interp(x, self.indicators['Coliformes Termotolerantes'][0], self.indicators['Coliformes Termotolerantes'][1]) if x <= 1e5 else 3 
-        self.indicatorFunctions['pH'] = lambda x: np.interp(x, self.indicators['pH'][0], self.indicators['pH'][1]) 
+        self.indicatorFunctions['pH'] = self.handlerPH
         self.indicatorFunctions['DBO (5, 20)'] = lambda x: np.interp(x, self.indicators['DBO (5, 20)'][0], self.indicators['DBO (5, 20)'][1]) if x <= 30 else 2
         self.indicatorFunctions['Nitrogênio-Nitrato'] = lambda x: np.interp(x, self.indicators['Nitrogênio-Nitrato'][0], self.indicators['Nitrogênio-Nitrato'][1]) if x <= 100 else 1
         self.indicatorFunctions['Fósforo Total'] = lambda x: np.interp(x, self.indicators['Fósforo Total'][0], self.indicators['Fósforo Total'][1]) if x <= 10 else 1
-        self.indicatorFunctions['Temperatura da Água'] = lambda x: np.interp(x, self.indicators['Temperatura da Água'][0], self.indicators['Temperatura da Água'][1])
+        self.indicatorFunctions['Temperatura da Água'] = self.handlerTemp
         self.indicatorFunctions['Turbidez'] = lambda x: np.interp(x, self.indicators['Turbidez'][0], self.indicators['Turbidez'][1]) if x <= 100 else 5
         self.indicatorFunctions['Sólido Dissolvido Total'] = lambda x: np.interp(x, self.indicators['Sólido Dissolvido Total'][0], self.indicators['Sólido Dissolvido Total'][1]) if x <= 500 else 32
         self.indicatorFunctions['Oxigênio Dissolvido'] = lambda x: np.interp(x, self.indicators['Oxigênio Dissolvido'][0], self.indicators['Oxigênio Dissolvido'][1]) if x <= 140 else 47
         self.indicatorList = list(self.indicators.keys())
         self.indicatorWeights = {a:b for a,b in zip(self.indicatorList, [0.15, 0.12, 0.1, 0.1, 0.1, 0.1, 0.08, 0.08, 0.17])}
        
+    def handlerPH(self, value):
+        if value<2:
+            return 2
+        elif value>12:
+            return 3
+        return np.interp(value, self.indicators['pH'][0], self.indicators['pH'][1])
+
+    def handlerTemp(self, value):
+        if value<-5:
+            return self.handlerTemp(-5)
+        elif value>15:
+            return 9
+        return np.interp(value, self.indicators['Temperatura da Água'][0], self.indicators['Temperatura da Água'][1])
+
     def IQA_Result(self, valuesDict: dict):
         keys = valuesDict.keys()
         result = 1
